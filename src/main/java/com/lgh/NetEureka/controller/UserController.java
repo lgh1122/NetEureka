eb78528bfc6349c725e1b2b31945ebef8b15527d
lghhome <1464070939@qq.com>
2019-06-10 22:44:28 +0800

    Initial commit

---
+++ src/main/java/com/lgh/NetEureka/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.lgh.NetEureka.controller;
+
+import com.lgh.NetEureka.entity.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/user")
+    @ResponseBody
+    public User sayHello(){
+        final User user = new User();
+        user.setName("Tom");
+        return user;
+    }
+}
